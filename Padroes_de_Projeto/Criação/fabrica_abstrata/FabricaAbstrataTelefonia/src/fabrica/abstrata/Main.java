03ee93ad048cb8ec5045288b849d2ce89b025e54
JuliaMRC <20171BSI0162@SR6739.cefetes.br>
2019-08-26 11:18:20 -0300

    Acrescentando metodo fabrica e operadora claro

--- Padroes_de_Projeto/Criação/fabrica_abstrata/FabricaAbstrataTelefonia/src/fabrica/abstrata/Main.java
+++ Padroes_de_Projeto/Criação/fabrica_abstrata/FabricaAbstrataTelefonia/src/fabrica/abstrata/Main.java
@@ -18,17 +18,25 @@ public class Main {
      */
     public static void main(String[] args) {
         
-        String escolha = JOptionPane.showInputDialog("Tim ou Vivo?");
+        String escolha = JOptionPane.showInputDialog("Tim, Vivo ou Claro?");
         int telefone = Integer.parseInt(JOptionPane.showInputDialog("Telefone?"));
         String texto = JOptionPane.showInputDialog("Mensagem");
         Mensagem msg = new Mensagem(texto);                
-        FabricaAbstrataEnvios fabrica = null;                              
-        if(escolha.equalsIgnoreCase("tim")){
+        
+        //FabricaAbstrataEnvios fabrica = null;                              
+        
+        /*if(escolha.equalsIgnoreCase("tim")){
             fabrica = new FabricaTim();
         }else if(escolha.equalsIgnoreCase("vivo")){
             fabrica = new FabricaVivo();            
-        }        
+        }else if(escolha.equalsIgnoreCase("claro")){
+            fabrica = new FabricaClaro();
+        }*/
+        
+        FabricaAbstrataEnvios fabrica = FabricaMensagem.gera(escolha);
+        
         EnvioSMS envio = fabrica.criaEnvioSMS();
+        
         if(envio.enviar(msg, telefone)){            
             Cobranca cobranca = fabrica.criaCobranca();
             cobranca.cobrar(telefone);
